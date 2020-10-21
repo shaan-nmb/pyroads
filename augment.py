d8c82d09e0050b1651b750b13621b839fff92920
shaan-nmb <shaan.ciantar@mainroads.wa.gov.au>
2020-10-21 13:10:40 +0000

    Fixed error on line 31 of reshape.py where the was no close parenthesis.

--- augment.py
+++ augment.py
@@ -10,22 +10,22 @@ def rurality(data, RA, road_no, SLK = "SLK") :
     return regionType
 
 ##Assign a natural key based on key variables
-def natural_key(data, SLK = 'SLK', trueSLK = 'TRUE_SLK', keyVars = []):
+def natural_key(data, SLK = 'SLK', trueSLK = 'TRUE_SLK', key_vars = []):
 
     #Create a natural key based on SLK and ID variables
-    newData = data.copy()
-    newData.insert(0, "NATURAL_KEY", "")
-    for var in keyVars:
-        newData["NATURAL_KEY"] += newData[var].astype(str) + '-'
-    newData["NATURAL_KEY"] = newData["NATURAL_KEY"] + newData[SLK].astype(str) + '-' + newData[trueSLK].astype(str)   
+    new_data = data.copy()
+    new_data.insert(0, "NATURAL_KEY", "")
+    for var in key_vars:
+        new_data["NATURAL_KEY"] += new_data[var].astype(str) + '-'
+    new_data["NATURAL_KEY"] = new_data["NATURAL_KEY"] + new_data[SLK].astype(str) + '-' + new_data[trueSLK].astype(str)   
 
-    return newData
+    return new_data
 
 ##Function that allows one to add columns to a dataset from one with a shared natural key.  
-def concat_cols(left, right, NaturalKey = "NATURAL_KEY", newCols = []):
-    newCols.append(NaturalKey)
-    concatData = left.merge(right[newCols], how = 'left', on = NaturalKey, suffixes = ('','_right'))
-    return concatData
+def concat_cols(left, right, naturalkey = "NATURAL_KEY", newCols = []):
+    newCols.append(naturalkey)
+    concat_data = left.merge(right[newCols], how = 'left', on = naturalkey, suffixes = ('','_right'))
+    return concat_data
 
 ###Find points of equation and add the information to the dataset
 # def pointOfEquation(data, SLK, locationVars)
--- reshape.py
+++ reshape.py
@@ -1,38 +1,38 @@
 ##Deal with a SLK column as metres in integers to avoid the issue of calculating on floating numbers
-def asMetres(var):
+def asmetres(var):
 
     m = round(var*1000).astype(int) 
 
     return m
 
 ##Turn each observation into sections of specified lengths
-def stretch(data, start, end, trueStart, trueEnd, obsLength = 10, dropVars = []):
+def stretch(data, start, end, true_start, true_end, obs_length = 10, dropvars = []):
 
-    newData = data.copy() #Copy of the dataset
-    newData = newData.dropna(thresh = 2)  #drop any row that does not contain at least two non-missing values.
+    new_data = data.copy() #Copy of the dataset
+    new_data = new_data.dropna(thresh = 2)  #drop any row that does not contain at least two non-missing values.
 
-    #Change start, end, and trueStart variables to 32 bit integers of metres to avoid the issue with calculations on floating numbers
-    newData[start] = asMetres(newData[start])
-    newData[end] = asMetres(newData[end])
-    newData[trueStart] = asMetres(newData[trueStart])
+    #Change start, end, and true_start variables to 32 bit integers of metres to avoid the issue with calculations on floating numbers
+    new_data[start] = asmetres(new_data[start])
+    new_data[end] = asmetres(new_data[end])
+    new_data[true_start] = asmetres(new_data[true_start])
 
-    #Reshape the data into size specified in 'obsLength'
-    newData = newData.reindex(newData.index.repeat(newData[end]/obsLength - newData[start]/obsLength)) #reindex by the number of intervals of specified length between the start and the end.
-    newData['SLK'] = newData[start] + newData.groupby(level=0).cumcount()*obsLength #Create SLK point column based on start plus the cumulative count of how many intervals away it is multiplied by the specified observation length. 
-    newData['TRUE_SLK'] = newData[trueStart] + newData.groupby(level=0).cumcount()*obsLength #Create SLK point column based on start plus the cumulative count of how many intervals away it is multiplied by the specified observation length.
+    #Reshape the data into size specified in 'obs_length'
+    new_data = new_data.reindex(new_data.index.repeat(new_data[end]/obs_length - new_data[start]/obs_length)) #reindex by the number of intervals of specified length between the start and the end.
+    new_data['SLK'] = new_data[start] + new_data.group_by(level=0).cumcount()*obs_length #Create SLK point column based on start plus the cumulative count of how many intervals away it is multiplied by the specified observation length. 
+    new_data['TRUE_SLK'] = new_data[true_start] + new_data.group_by(level=0).cumcount()*obs_length #Create SLK point column based on start plus the cumulative count of how many intervals away it is multiplied by the specified observation length.
     
     #Convert metre references back to kilometres
-    newData['SLK'] = newData['SLK']/1000
-    newData['TRUE_SLK'] = newData['TRUE_SLK']/1000
+    new_data['SLK'] = new_data['SLK']/1000
+    new_data['TRUE_SLK'] = new_data['TRUE_SLK']/1000
     
-    newData = newData.reset_index(drop = True) 
+    new_data = new_data.reset_index(drop = True) 
     
-    #Drop variables no longer required
-    for var in dropVars + [start, end, trueStart, trueEnd]:
-        newData.drop([var], axis = 1, inplace = True)
-    newData = newData.reset_index(drop = True)
+    #Drop the variables no longer required
+    for var in (dropvars + [start, end, true_start, true_start]):
+        new_data.drop([var], axis = 1, inplace = True)
+    new_data = new_data.reset_index(drop = True)
     
-    return newData
+    return new_data
 
 ##Function that allows for the merging of two datasets with shared observation lengths on all mutual columns
 def fuse(datasets = [], ignore = None, join = 'inner'):
@@ -42,66 +42,66 @@ def fuse(datasets = [], ignore = None, join = 'inner'):
         return
         
     #Determine which columns to merge over
-    allColumns = [] #initialise empty list
+    all_columns = [] #initialise empty list
     for dataset in datasets:
-        allColumns = allColumns + list(dataset.columns.values) #Add the column names iteratively from each dataset
-    sharedColumns = [x for x in allColumns if allColumns.count(x) >= len(datasets)]  #Identify which columns appear in all the datasets
+        all_columns = all_columns + list(dataset.columns.values) #Add the column names iteratively from each dataset
+    shared_columns = [x for x in all_columns if all_columns.count(x) >= len(datasets)]  #Identify which columns appear in all the datasets
     
     if ignore != None:
         for i in ignore:
-            if i in sharedColumns:
-                sharedColumns.remove(i)
+            if i in shared_columns:
+                shared_columns.remove(i)
     
     #loop the specified join based on the shared columns
     for i in range(len(datasets)):
         while i <= (len(datasets) - 1):
-            newData = datasets[i-1].merge(datasets[i], how = join, on= sharedColumns)
+            new_data = datasets[i-1].merge(datasets[i], how = join, on= shared_columns)
             i+=1 
-    return newData
+    return new_data
 
 ##Function that Compact 10m data so that it has 'from' and 'to' SLK columns 
-def compact(data, SLK = "SLK", trueSLK = "TRUE_SLK", obsLength = 10, idVars = [], groupBy = []):
+def compact(data, SLK = "SLK", true_SLK = "TRUE_SLK", obs_length = 10, idvars = [], group_by = []):
 
     import numpy as np
 
-    #Sort by all columns in groupBy, then by true SLK, then by SLK
-    newData = data.copy().sort_values(idVars + [SLK]).reset_index(drop = True)
+    #Sort by all columns in group_by, then by true SLK, then by SLK
+    new_data = data.copy().sort_values(idvars + [SLK]).reset_index(drop = True)
 
-    #Create a column that is a concatenation of all the columns in the groupBy
-    newData.insert(0, "groupKey", "")
-    for var in groupBy + idVars:
-        newData["groupKey"] += newData[var].astype(str) + '-'
+    #Create a column that is a concatenation of all the columns in the group_by
+    new_data.insert(0, "groupkey", "")
+    for var in group_by + idvars:
+        new_data["groupkey"] += new_data[var].astype(str) + '-'
 
-    #Convert SLK and trueSLK columns into integers of metres to avoid the problems caused by calculations on floating numbers
-    newData[SLK] = asMetres(newData[SLK])
-    newData[trueSLK] = asMetres(newData[trueSLK])
+    #Convert SLK and true_SLK columns into integers of metres to avoid the problems caused by calculations on floating numbers
+    new_data[SLK] = asmetres(new_data[SLK])
+    new_data[true_SLK] = asmetres(new_data[true_SLK])
 
     #Create lag and lead columns for SLK, true SLK, and the grouping key to check whether a new group has started
-    newData['leadSLK'] = newData[SLK].shift(-1, fill_value = 1000)
-    newData['lagSLK'] = newData[SLK].shift(1, fill_value = 1000)
-    newData['leadTrueSLK'] = newData[trueSLK].shift(-1, fill_value = 1000)
-    newData['lagTrueSLK'] = newData[trueSLK].shift(1, fill_value = 1000)
-    newData['leadGroupKey'] = newData['groupKey'].shift(-1, fill_value = 'End')
-    newData['lagGroupKey'] = newData['groupKey'].shift(1, fill_value = 'Start')
+    new_data['leadSLK'] = new_data[SLK].shift(-1, fill_value = 1000)
+    new_data['lagSLK'] = new_data[SLK].shift(1, fill_value = 1000)
+    new_data['leadtrue_SLK'] = new_data[true_SLK].shift(-1, fill_value = 1000)
+    new_data['lagtrue_SLK'] = new_data[true_SLK].shift(1, fill_value = 1000)
+    new_data['leadgroupkey'] = new_data['groupkey'].shift(-1, fill_value = 'End')
+    new_data['laggroupkey'] = new_data['groupkey'].shift(1, fill_value = 'Start')
 
     #Create columns based on whether they represent the start or end of a section.
 
     #if the lagged SLKs are not one observation length less than the actual or the lagged group-key is different.
-    start = np.where((newData['lagSLK'] == newData[SLK] - obsLength) & (newData['lagTrueSLK'] == newData[trueSLK] - obsLength) & (newData['lagGroupKey'] == newData['groupKey']), False,True)
+    start = np.where((new_data['lagSLK'] == new_data[SLK]-obs_length) & (new_data['lagtrue_SLK'] == new_data[true_SLK]-obs_length) & (new_data['laggroupkey'] == new_data['groupkey']), False,True)
     #if the lead SLKs are not one observation length more than the actual or the lead group-key is different.
-    end   = np.where((newData['leadSLK'] == newData[SLK] + obsLength) & (newData['leadTrueSLK'] == newData[trueSLK] + obsLength) & (newData['leadGroupKey'] == newData['groupKey']), False, True)
-    newData['start'] = start
-    newData['end'] = end
+    end   = np.where((new_data['leadSLK'] == new_data[SLK]+obs_length) & (new_data['leadtrue_SLK'] == new_data[true_SLK]+obs_length) & (new_data['leadgroupkey'] == new_data['groupkey']), False, True)
+    new_data['start'] = start
+    new_data['end'] = end
 
     #Create the compact dataset
 
-    compactData = newData.copy()[start][idVars + groupBy].reset_index(drop = True) #Data for the id and grouping variables for every start instance. 
-    compactData.insert(2, "START_SLK", newData[start].reset_index(drop = True)[SLK]) 
-    compactData.insert(4, "START_TRUE", newData[start].reset_index(drop = True)[trueSLK])
+    compact_data = new_data.copy()[start][idvars + group_by].reset_index(drop = True) #Data for the id and grouping variables for every start instance. 
+    compact_data.insert(2, "START_SLK", new_data[start].reset_index(drop = True)[SLK]) 
+    compact_data.insert(4, "START_TRUE", new_data[start].reset_index(drop = True)[true_SLK])
     #As each start instance has a corresponding end instance with shared grouping and id variables, it can be joined horizontally.
-    compactData.insert(3, "END_SLK", newData[end].reset_index(drop = True)[SLK] + obsLength) 
-    compactData.insert(5, "END_TRUE", newData[end].reset_index(drop = True)[trueSLK] + obsLength)
+    compact_data.insert(3, "END_SLK", new_data[end].reset_index(drop = True)[SLK] + obs_length) 
+    compact_data.insert(5, "END_TRUE", new_data[end].reset_index(drop = True)[true_SLK] + obs_length)
     
-    compactData = compactData.sort_values(idVars).reset_index(drop = True) #Sort data by the location varibles
-    return compactData
+    compact_data = compact_data.sort_values(idvars).reset_index(drop = True) #Sort data by the location varibles
+    return compact_data
     
