96a793f6e6331e0208c59ae73ce0acecbaecaee2
shaan-nmb <shaan.ciantar@mainroads.wa.gov.au>
2022-01-07 17:27:45 +0800

    Updated version number to allow for new install.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "pyroads",
-    version = "0.1.5",
+    version = "0.1.6",
     packages = find_packages(),
     description = "For analysis of road asset data.",
     url = "https://github.com/shaan-nmb/pyroads",
